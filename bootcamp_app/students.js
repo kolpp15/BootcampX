9c76a7347ad5b22b3a6fea46aa970d47db3b1f02
Brian Sohn <kolpp15@gmail.com>
2021-08-18 18:12:42 -0700

    using pool.query to use JS and SQL in command line

---
+++ bootcamp_app/students.js
@@ -0,0 +1,73 @@
+// Another way to specify connection:
+// const pool = new Pool({
+//   user: 'vagrant',
+//   password: '123',
+//   host: 'localhost',
+//   database: 'bootcampx'
+// });
+
+const { Pool } = require('pg');
+const pool = new Pool();
+
+pool.connect()
+.then(() => {
+  console.log('db connected');
+})
+.catch((err) => {
+  console.log('db connection error:', err.stack);
+});
+
+
+// ----------------------------BASIC SQL USING JS to print out the RESULT ROWS
+// pool.query(`
+// SELECT id, name, cohort_id
+// FROM students
+// LIMIT 5;
+// `)
+//   .then(res => {
+//     console.log(res.rows);
+//   })
+//   .catch(err => console.error('query error', err.stack));
+
+// ---------------------------- USING JS TO CONSOLE LOG with SQL
+// pool.query(`
+// SELECT id, name, cohort_id
+// FROM students
+// LIMIT 5;
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
+//   })
+// });
+
+// ---------------------------- CONTINUE OF THE ABOVE
+// pool.query(`
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// LIMIT 5;
+// `)
+//   .then(res => {
+//     res.rows.forEach(user => {
+//       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//     });
+//   });
+
+// ----------------------------- USING PROCESS.ARGV
+
+const cohortName = process.argv[2];
+const maxResult = process.argv[3];
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${cohortName}%'
+LIMIT ${maxResult || 5};
+`)
+.then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
+  }).catch(err => console.error('query error', err.stack));
